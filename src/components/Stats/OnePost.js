2d4cb03e319097112093cbc71ac7013e3e1df577
Nan G <nandini.govindarajan@gmail.com>
2022-01-05 04:05:24 -0500

    optimized blogpost page

--- src/components/Stats/OnePost.js
+++ src/components/Stats/OnePost.js
@@ -1,18 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import imageUrlBuilder from '@sanity/image-url';
 import BlockContent from '@sanity/block-content-to-react';
 import dayjs from 'dayjs';
 import sanityClient from '../../client';
 
-const builder = imageUrlBuilder(sanityClient);
-function urlFor(source) {
-  return builder.image(source);
-}
-
 export default function OnePost() {
   const [postData, setPostData] = useState(null);
-  const query = '*[slug.current == $slug]{ title, publishedAt, slug, mainImage{asset->{_id,url}}, body, "name": author->name, "authorImage": author->image}';
+  const query = '*[slug.current == $slug]{ title, publishedAt, slug, body, "name": author->name, "authorImage": author->image}';
   const { slug } = useParams();
 
   useEffect(() => {
@@ -29,7 +23,6 @@ export default function OnePost() {
       <div className="post-heading">
         <p className="post-date">{dayjs(postData.publishedAt).format('MMMM DD, YYYY')}</p>
         <h3 className="post-title">{postData.title}</h3>
-        <img className="post-image" src={urlFor(postData.mainImage).width(200).url()} alt="main one" />
       </div>
       <div className="post-body">
         <BlockContent
